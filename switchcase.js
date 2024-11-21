70e43c9d50f17fee856ce05322ddae366c9327ce
lokimoli <shauryasundaram7756@gmail.com>
2024-11-21 21:51:02 +0530

    Switch case statements

---
+++ switchcase.js
@@ -0,0 +1,31 @@
+const cups = 47234;
+switch (cups) {
+  case 4:
+    console.log("The value of cups is 4");
+    break;
+
+  case 43:
+    console.log("The value of cups is 43");
+    break;
+
+  case 44:
+    console.log("The value of cups is 44");
+    break;
+
+  case 45:
+    console.log("The value of cups is 45");
+    break;
+
+  case 46:
+    console.log("The value of cups is 46");
+    break;
+
+  case 47:
+    console.log("The value of cups is 47");
+    break;
+
+  default:
+    console.log("The value of cups is none of 4,43,44,45,46,47");
+    
+    break;
+}
