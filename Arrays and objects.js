77bad11632ae76456f6bcce66ab703ade530c421
lokimoli <shauryasundaram7756@gmail.com>
2024-11-22 23:00:52 +0530

    Functions , Arays and objects done

---
+++ Arrays and objects.js
@@ -0,0 +1,21 @@
+let myVar = 34;
+let myVar2 = "String";
+let myVar3 = true;
+let myVar4 = null;
+let myVar5 = undefined;
+
+let employee = {
+    name: "Harry",
+    salary: 1000000,
+    class: 10,
+}
+
+console.log((employee));
+
+// Arrays - objects
+let names = [1 , 2 , 3 , 4 ,"Shaurya" , undefined]
+console.log(name);
+// can aslo write the value in [] bordered by 9 
+//  eg : console.log(name[0])  output = 1
+
+
--- functions.js
+++ functions.js
@@ -3,15 +3,61 @@
     return "hi"
  }
  
- function onePlusAvg(x,y){
-    return Math.round(1+(x+y)/2)
+//  function onePlusAvg(x,y){
+//     return Math.round(1+(x+y)/2)
+//  }
+
+//  let a = 1;
+//  let b = 2;
+//  let c = 3;
+//  hello();
+
+//  console.log("One plus average of a and b is ", onePlusAvg(a,b));
+//  console.log("One plus average of b and c is ", onePlusAvg(b,c));
+//  console.log("One plus average of c and a is ", onePlusAvg(a,c));
+
+/*
+ function greet(name,greetText){
+         //  console.log(greetText + " " + name);
+          console.log(greetText + " " + name + " is a good boy")
  }
 
- let a = 1;
- let b = 2;
- let c = 3;
- hello();
+ let greetText = "Good morning"
+ let name = "Shaurya";
+ let name1 = "Yug";
+ let name2 = "Harsh;it"
+ let name3 = "Sam"
+ let name4 = "virru"
 
- console.log("One plus average of a and b is ", onePlusAvg(a,b));
- console.log("One plus average of b and c is ", onePlusAvg(b,c));
- console.log("One plus average of c and a is ", onePlusAvg(a,c));
+ greet(name , greetText);
+ greet(name1 , greetText);
+ greet(name2 , greetText);
+ greet(name3 , greetText);
+ greet(name4 , greetText);
+
+// The variable which is defined first should also come first in the function or else the output will show "Not defined"
+
+ console.log(name + "is a good boy");
+ console.log(name + "is a good boy");
+ console.log(name + "is a good boy");
+ console.log(name + "is a good boy");
+ console.log(name + "is a good boy");
+
+
+ function sum(a,b,c){
+          let d = a-b-c;
+          return d;
+ }
+
+ let value = sum(214214,34135,3523)
+ console.log(value);
+*/
+
+ function average(a,b,c){
+   let d = (a+b+c)/3
+   return d;
+ }
+
+ let avg = average(10,20,30);
+ console.log(avg);
+ 
